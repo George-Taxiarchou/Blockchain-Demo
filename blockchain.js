08433ec25b0257155643b749587db76495ccad31
Gtax <gtax@Gtaxs-MacBook-Pro.local>
2018-02-06 18:36:44 +0200

    added transactions

--- blockchain.js
+++ blockchain.js
@@ -1,50 +1,86 @@
 const SHA256 = require("crypto-js/sha256");
+
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = '') {
         this.previousHash = previousHash;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
-    mineBlock(difficulty) {
-	    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-	        this.nonce++;
-	        this.hash = this.calculateHash();
-	    }
-	    console.log("BLOCK MINED: " + this.hash);
-	}
 
     calculateHash() {
-	    return SHA256(this.index +
-	        this.previousHash +
-	        this.timestamp +
-	        JSON.stringify(this.data) +
-	        this.nonce
-	    ).toString();
-	}
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("BLOCK MINED: " + this.hash);
+    }
 }
+
 
 class Blockchain{
     constructor() {
-	    this.chain = [this.createGenesisBlock()];
-	    this.difficulty = 1;
-	}
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
+    }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/2018", "Genesis block", "0");
+        return new Block(Date.parse("2018-01-01"), [], "0");
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-	    newBlock.previousHash = this.getLatestBlock().hash;
-	    newBlock.mineBlock(this.difficulty);
-	    this.chain.push(newBlock);
-	}
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
+    }
 
     isChainValid() {
         for (let i = 1; i < this.chain.length; i++){
@@ -59,21 +95,22 @@ class Blockchain{
                 return false;
             }
         }
+
         return true;
     }
 }
 
-let darkyCoin = new Blockchain();
-darkyCoin.addBlock(new Block(1, Date.now(), { amount: 4 }));
-darkyCoin.addBlock(new Block(2, Date.now(), { amount: 8 }));
+let kappaCoin = new Blockchain();
+kappaCoin.createTransaction(new Transaction('address1', 'address2', 100));
+kappaCoin.createTransaction(new Transaction('address2', 'address1', 50));
 
-// Check if chain is valid (will return true)
-console.log('Blockchain valid? ' + darkyCoin.isChainValid());
+console.log('\n Starting the miner...');
+kappaCoin.minePendingTransactions('kappas-address');
 
-//  manipulate the data
-darkyCoin.chain[1].data = { amount: 100 };
+console.log('\nBalance of kappa is', kappaCoin.getBalanceOfAddress('kappas-address'));
 
-// Check our chain again (will now return false)
-console.log("Blockchain valid? " + darkyCoin.isChainValid());
+console.log('\n Starting the miner again...');
+kappaCoin.minePendingTransactions('kappas-address');
 
+console.log('\nBalance of kappa is', kappaCoin.getBalanceOfAddress('kappas-address'));
 
